de13b8b9b27a3fc258473d6fb44b005727083f0b
Rafael Bidese <rafaelbidese@gmail.com>
2020-04-14 22:52:31 -0500

    Update latest results

--- lumberjack_net.py
+++ lumberjack_net.py
@@ -12,7 +12,11 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
+from torchvision import transforms
 import argparse
+
+from scipy import signal
+from scipy.io import wavfile
 
 torch.manual_seed(0)
 torch.cuda.manual_seed(0)
@@ -26,26 +30,63 @@ def _json_object_hook(d):
 def json2obj(data):
     return json.loads(data, object_hook=_json_object_hook)
 
+
+def calcMeanStdDataset(dataset):
+    trees = np.empty([1,1])
+    for tree in dataset:
+        trees = np.concatenate((trees, tree.mfccs.reshape(1,-1)), axis=None)
+    print(np.mean(trees), np.std(trees))
+
+
+class TreeData():
+    def __init__(self, dbh, filepath, mfccs):
+        self.dbh = dbh
+        self.filepath = filepath
+        self.mfccs = mfccs
+
 class TreeCutDataset(Dataset):
-    def __init__(self, is_train=True, n_mfcc=40, n_fft=1024, hop_length=512):
+    def __init__(self, is_train=True, n_mfcc=40, n_fft=1024, hop_length=512, transform=None):
         path = 'train_split.json' if is_train else 'val_split.json'
         with open(path, 'r') as json_file:
             json_data = json_file.read()
             trees = json.loads(json_data)
-            self.dataset = json2obj(json.dumps(trees))
+            dataset = json2obj(json.dumps(trees))
         self.n_mfcc = n_mfcc
         self.n_fft = n_fft
         self.hop_length = hop_length
+        self.transform = transform
+        self.dataset = []
+
+        for i,sample in enumerate(dataset):
+            dbh = sample.DBH
+            filepath = ''.join(['data',os.sep,sample.directory,os.sep,sample.fileName])
+            data, sample_rate = librosa.load(filepath)
+            # print(sample_rate)
+            mfccs = librosa.feature.mfcc(y=data, sr=48000, n_fft=self.n_fft, n_mfcc = self.n_mfcc,hop_length=self.hop_length)
+            # mfccs = librosa.feature.mfcc(y=data, sr=sample_rate, dct_type=3, n_fft=self.n_fft, n_mfcc = self.n_mfcc,hop_length=self.hop_length)
+            # mfccs = np.abs(librosa.core.stft(y=data, n_fft=self.n_fft, hop_length=None, win_length=None, window='hann', center=True, pad_mode='reflect'))
+
+            # sample_rate, samples = wavfile.read(filepath)
+            # frequencies, times, spectrogram = signal.spectrogram(samples, sample_rate)
+
+            # plt.pcolormesh(times, frequencies, spectrogram)
+            # plt.imshow(spectrogram)
+            # plt.ylabel('Frequency [Hz]')
+            # plt.xlabel('Time [sec]')
+            # plt.show()
+
+            self.dataset.append(TreeData(dbh, filepath, mfccs))
+        
+        # calcMeanStdDataset(self.dataset)
 
     def __len__(self):
         return len(self.dataset)
 
     def __getitem__(self, index):
-        self.dbh = self.dataset[index].DBH
-        self.filepath = ''.join(['data',os.sep,self.dataset[index].directory,os.sep,self.dataset[index].fileName])
-        data, sample_rate = librosa.load(self.filepath)
-        self.mfccs = librosa.feature.mfcc(y=data, sr=sample_rate, n_fft=self.n_fft, n_mfcc = self.n_mfcc,hop_length=self.hop_length)
-        return self.dbh, self.filepath, self.mfccs
+        data = self.dataset[index]
+        if self.transform is not None:
+            data.mfccs = self.transform(data.mfccs)
+        return data.dbh, data.filepath, data.mfccs
 
 def show_files(dataloader):
     fig = plt.figure(figsize=(15,15))
@@ -60,12 +101,12 @@ def show_files(dataloader):
         break
 
 def show_mfcc(dataloader):
-    plt.figure(figsize=(8,8))
+    plt.figure(figsize=(10,6))
     for i, (dbhs, files, mfccs) in enumerate(dataloader):
         for j, (dbh, file, mfcc) in enumerate(zip(dbhs, files, mfccs)):
             # print(mfcc.numpy().size)
             data, sample_rate = librosa.load(file)
-            librosa.display.specshow(mfcc.numpy(), sr=sample_rate, x_axis='time')
+            librosa.display.specshow(mfcc.numpy(), sr=22050, x_axis='time')
             plt.title(''.join([file, ',', str(dbh.item())]))
             plt.savefig('MFCCs.png')
         break
@@ -78,37 +119,56 @@ def show_sample(model, device, dataloader_train):
     show_files(dataloader_train)
 
 class LumberjackNet(nn.Module):
-    def __init__(self):
+    def __init__(self,p=0.5,chan1=32,kernel=5, hop_length=512, n_mfcc=40, n_fft=512):
         super(LumberjackNet, self).__init__()
+        self.hop_length = hop_length
+        self.n_mfcc = n_mfcc
+        self.n_fft = n_fft
+        self.signal_length = 72050
+        self.inputH = 141
+        self.p = p
+        self.kernel = kernel
         # channels, output, kernels 
-        self.conv1 = nn.Conv2d(1,6,5)
+        self.conv1 = nn.Conv2d(1,chan1,kernel)
         # kernel size, stride
-        self.pool = nn.MaxPool2d(2,2)
-        # # channels, output, kernels 
-        self.conv2 = nn.Conv2d(6,18,5)
-        self.fc1 = nn.Linear(18*7*32,100)
-        self.fc2 = nn.Linear(100,10)
-        self.fc3 = nn.Linear(10,1)
+        self.pool = nn.MaxPool2d(2,2) # chan1, n_mfcc-k+1, 141-k+1
+        # # channels, output, kernels # 32,18,67  
+        self.conv2W     = int((self.n_mfcc-self.kernel+1) / 2)
+        self.conv2H     = int((self.inputH-self.kernel+1) / 2)
+        self.conv2      = nn.Conv2d(chan1,self.conv2W,kernel) # channels, 40 - kernel%2 / pool, 141 - kernel%2 / pool
+        self.fc1mul2    = int((self.conv2W-self.kernel+1)/2)
+        self.fc1uml3    = int((self.conv2H-self.kernel+1)/2)
+        self.fc1        = nn.Linear(self.conv2W*self.fc1mul2*self.fc1uml3,100)
+        self.fc2        = nn.Linear(100,10)
+        self.fc3        = nn.Linear(10,1)
+        self.drop       = nn.Dropout(p=p)
 
     def forward(self,x):
-        x=x.view(-1,1,40,141)
-        # -> n, 1, 40, 141
+        x=x.view(-1,1,self.n_mfcc,self.inputH)                # -> n, 1, 40, 141
         x = self.pool(F.relu(self.conv1(x))) # -> n, 6, 18, 68
         x = self.pool(F.relu(self.conv2(x))) # -> n, 18, 7, 32
-        x = x.view(-1,18*7*32)               # -> n, 18*7*32
+        x = x.view(-1,self.conv2W*self.fc1mul2*self.fc1uml3)               # -> n, 18*7*32
         x = F.relu(self.fc1(x))              # -> n, 100
         x = F.relu(self.fc2(x))              # -> n, 84
         x = self.fc3(x)                      # -> n, 1
         return x
 
 def train(model, device,criterion,optimizer,n_epochs,dataloader_train, dataloader_test, exp):
-
+    # TODO: add train loss to plot
     all_mse = []
     all_r2 = []
+
+    all_train_mse = []
+    all_train_r2 = []
     best_model_r2 = -100;
     mse = 0
     for epoch in tqdm(range(n_epochs), desc="TRAIN"):
+        model.train()
+        accloss = 0
+        all_train_pred = np.zeros([])
+        all_train_dbhs = np.zeros([])
         for i, (dbhs, files, mfccs) in enumerate(dataloader_train):
+            optimizer.zero_grad()
             # forward pass and loss
             dbhs = dbhs.view(-1,1).float()
             dbhs = dbhs.to(device)
@@ -119,28 +179,45 @@ def train(model, device,criterion,optimizer,n_epochs,dataloader_train, dataloade
             loss.backward()
             # updates
             optimizer.step()
-            optimizer.zero_grad()
+
+            accloss += loss.cpu().detach().numpy()
+
+            all_train_dbhs = np.append(all_train_dbhs, dbhs.cpu().detach().numpy()[:,0])
+            all_train_pred = np.append(all_train_pred,  pred.cpu().detach().numpy()[:,0])
+    
+            
+
+        mse = accloss.sum() / len(dataloader_train)
+        r2score = r2_score(all_train_dbhs[1:], all_train_pred[1:])
+        all_train_r2.append(r2score)
+        all_train_mse.append(mse)
+        time = strftime("%Y-%m-%d %H:%M:%S")
+        if (epoch+1)%1 == 0:
+            print(f'\n{time} [TRAIN] {epoch+1}] mse = {mse:.4f} r2score = {r2score:.4f}')
+    
+    # torch.save(model.state_dict(), f'./{exp}/model/checkpoint.pth')
 
         accloss = 0
         all_pred = np.zeros([])
         all_dbhs = np.zeros([])
         ## VALIDATION PER EPOCH
         with torch.no_grad():
+            model.eval()
             for i, (dbhs, files, mfccs) in enumerate(dataloader_test):
                 dbhs = dbhs.view(-1,1).float()
                 dbhs = dbhs.to(device)
                 mfccs = mfccs.to(device)
                 pred = model(mfccs)
                 loss = criterion(pred, dbhs)
-                accloss += loss
+                accloss += loss.cpu().numpy()
                 all_dbhs = np.append(all_dbhs, dbhs.cpu().numpy()[:,0])
                 all_pred = np.append(all_pred,  pred.cpu().numpy()[:,0])
 
-        mse = accloss.sum() / len(dataloader_test)
-        r2score = r2_score(all_dbhs[1:], all_pred[1:])
-        # all_losses.append(mse.cpu().numpy())
-        all_r2.append(r2score)
-        all_mse.append(mse)
+            mse = accloss.sum() / len(dataloader_test)
+            r2score = r2_score(all_dbhs[1:], all_pred[1:])
+            # all_losses.append(mse.cpu().numpy())
+            all_r2.append(r2score)
+            all_mse.append(mse)
 
         if (r2score > best_model_r2):
             torch.save(model.state_dict(), f'./{exp}/model/checkpoint.pth')
@@ -148,24 +225,26 @@ def train(model, device,criterion,optimizer,n_epochs,dataloader_train, dataloade
 
         time = strftime("%Y-%m-%d %H:%M:%S")
         if (epoch+1)%1 == 0:
-            print(f'\n{time} [EPOCH {epoch+1}] mse = {mse:.4f} r2score = {r2score:.4f}')
+            print(f'{time} [TRAIN VAL] {epoch+1}] mse = {mse:.4f} r2score = {r2score:.4f}')
 
-    # LOG R2 SCORES
-    with open(f'./{exp}/logs/train_r2_loss_log.txt', 'w') as log:
+    # LOG VAL R2 SCORES
+    with open(f'./{exp}/logs/train_val_r2_loss_log.txt', 'w') as log:
         wr = csv.writer(log)
         wr.writerows(zip(all_r2))
     plt.figure()
     plt.plot(all_r2, 'bo')
-    plt.title("TRAIN R2 LOSS")
-    plt.savefig(f'./{exp}/logs/train_r2_loss_log.png')
+    plt.plot(all_train_r2, 'ro')
+    plt.title("TRAIN VAL R2 LOSS")
+    plt.savefig(f'./{exp}/logs/train_val_r2_loss_log.png')
 
-    # LOG MSE SCORES
-    with open(f'./{exp}/logs/train_mse_loss_log.txt', 'w') as log:
+    # LOG VAL MSE SCORES
+    with open(f'./{exp}/logs/train_val_mse_loss_log.txt', 'w') as log:
         wr = csv.writer(log)
         wr.writerows(zip(all_mse))
     plt.figure()
     plt.plot(all_mse, 'bo')
-    plt.title("TRAIN MSE LOSS")
+    plt.plot(all_train_mse, 'ro')
+    plt.title("TRAIN VAL MSE LOSS")
     plt.savefig(f'./{exp}/logs/train_mse_loss_log.png')
 
 def evaluate_model(model,dataloader_test,device,criterion,exp):
@@ -174,6 +253,7 @@ def evaluate_model(model,dataloader_test,device,criterion,exp):
     all_dbhs = np.zeros([])
     mse = 0
     with torch.no_grad():
+        model.eval()
         for i in tqdm(range(len(dataloader_test)), desc="EVAL"):
             dbhs, files, mfccs = next(iter(dataloader_test))
             dbhs = dbhs.view(-1,1).float()
@@ -200,39 +280,47 @@ def evaluate_model(model,dataloader_test,device,criterion,exp):
     plt.plot(x,x)
     plt.savefig(f'./{exp}/logs/best_model_pred_log.png')
 
-def main(training,n_epochs, n_mfcc,n_fft, hop_length, batch_size,lr=0.00001,momentum=0.9, exp='exp01'):
+def main(training,n_epochs, n_mfcc,n_fft, hop_length, batch_size,lr=0.00001,momentum=0.9, weight_decay=0.01, p=0.5, exp='exp01', chan1=32, kernel=4):
     dataset_train = TreeCutDataset(is_train=True, n_mfcc=n_mfcc,n_fft=n_fft, hop_length=hop_length)
     dataset_test = TreeCutDataset(is_train=False, n_mfcc=n_mfcc,n_fft=n_fft, hop_length=hop_length)
+    
+
     dataloader_train = DataLoader(dataset_train, batch_size=batch_size, shuffle=True, num_workers=0)
     dataloader_test = DataLoader(dataset_test, batch_size=batch_size, shuffle=True, num_workers=0)
     
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    model = LumberjackNet().to(device)
+    model = LumberjackNet(p=p, chan1=chan1, kernel=kernel, hop_length=hop_length, n_mfcc=n_mfcc, n_fft=n_fft).to(device)
     criterion = nn.MSELoss()
-    optimizer = torch.optim.RMSprop(model.parameters(), lr=lr, momentum=momentum)
+    # criterion = nn.SmoothL1Loss()
+    # optimizer = torch.optim.RMSprop(model.parameters(), lr=lr, momentum=momentum,  weight_decay=weight_decay)
+    optimizer = torch.optim.SGD(model.parameters(), lr=lr, momentum=momentum,  weight_decay=weight_decay)
+    # optimizer = torch.optim.Adam(model.parameters(), lr=0.0001, betas=(0.9, 0.999), eps=1e-08, weight_decay=weight_decay, amsgrad=False)
     
     # show_sample(model, device, dataloader_train)
+    # show_mfcc(dataloader_train)
 
     if(training):
-        model.train()
         train(model, device,criterion,optimizer,n_epochs,dataloader_train, dataloader_test,exp)
     
     # EVALUATE WITH BEST MODEL
     model.load_state_dict(torch.load(f'./{exp}/model/checkpoint.pth'))
-    model.eval()
     evaluate_model(model,dataloader_test,device,criterion,exp)
     
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("--n_epochs", type=int, default=100, help="number of epochs of training")
+    parser.add_argument("--n_epochs", type=int, default=300, help="number of epochs of training")
     parser.add_argument("--batch_size", type=int, default=16, help="size of the batches")
-    parser.add_argument("--lr", type=float, default=0.00001, help="rmsprop: learning rate")
+    parser.add_argument("--lr", type=float, default=0.000001, help="rmsprop: learning rate")
     parser.add_argument("--momentum", type=float, default=0.9, help="rmsprop: momentum of gradient")
+    parser.add_argument("--weight_decay", type=float, default=0.9, help="rmsprop: weight decay l2-norm regularization")
+    # parser.add_argument("--p", type=float, default=0.25, help="dropout rate")
     parser.add_argument("--n_mfcc", type=int, default=40, help="number of coefficients")
     parser.add_argument("--n_fft", type=int, default=1024, help="size of the fft")
     parser.add_argument("--hop_length", type=int, default=512, help="window hop for mfcc")
-    parser.add_argument("--exp", type=str, default='exp00', help="experiment name")
+    parser.add_argument("--exp", type=str, default='exp01', help="experiment name")
+    parser.add_argument("--chan1", type=int, default=32, help="channels in cnn")
+    parser.add_argument("--kernel", type=int, default=8, help="kernel size")
     opt = parser.parse_args()
     print(opt)
 
@@ -250,4 +338,8 @@ if __name__ == '__main__':
         batch_size=opt.batch_size,
         lr=opt.lr,
         momentum=opt.momentum,
-        exp=opt.exp)
+        exp=opt.exp,
+        weight_decay=opt.weight_decay,
+        # p=opt.p,
+        chan1=opt.chan1,
+        kernel=opt.kernel)
--- train_script.sh
+++ train_script.sh
@@ -1,13 +1,45 @@
 #!/bin/bash
 
-declare -a lr_arr=(0.0001 0.00001 0.000001)
-declare -a btc_arr=(1 8 16 32 64)
+declare -a lr_arr=(0.00001 0.000001 0.0000001)
+declare -a btc_arr=(16 32 64)
+declare -a cha_arr=(8 16 32 64 128)
+declare -a m_ar=(0.1 0.5 0.9)
+declare -a fft_arr=(512 1024 2048)
+# declare -a p_arr=(0 0.25 0.5 0.75)
+declare -a wd_arr=(0.1 0.5 0.9)
+declare -a ker_arr=(2 4 6 8)
+declare -a mf_arr=(30 40 50)
 
 i=0
 
 for lr in "${lr_arr[@]}"; do
   for btc in "${btc_arr[@]}";do
-    python lumberjack_net.py --n_epoch 1 --lr "$lr" --momentum 0.9 --batch_size "$btc" --exp "exp$i"
-    ((i++))
+    for cha in "${cha_arr[@]}";do
+      for m in "${m_ar[@]}";do
+        for fft in "${fft_arr[@]}";do
+          # for p in "${p_arr[@]}";do
+            for wd in "${wd_arr[@]}";do
+              for k in "${ker_arr[@]}";do
+                for mf in "${mf_arr[@]}";do
+                ((i++))
+                python lumberjack_net.py          \
+                              --n_epoch 600         \
+                              --lr "$lr"            \
+                              --batch_size "$btc"   \
+                              --chan1 "$cha"        \
+                              --momentum "$m"       \
+                              --n_fft "$fft"        \
+                              --weight_decay "$wd"  \
+                              --kernel "$k"         \
+                              --n_mfcc "$mf"        \
+                              --exp "exp$i"         
+                      
+                done
+              done
+            done
+          # done
+        done
+      done
+    done
   done
 done
